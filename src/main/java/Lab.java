b56371871ea3d69ded6e4f106eab64999d597bdb
chnwachu20 <chnwachu@gmail.com>
2024-12-16 18:48:28 +0000

    Completed?

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,14 +1,13 @@
-
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Sets are unordered collections of elements. Sets do not allow duplicates.
  *
  * Sets are very efficient for storing items that should be unique.
- *
- * You can find more information about HashSet, a common and very efficient implementation of Set, by searching online.
  */
 public class Lab {
+
     /**
      * Instantiate and return a Set of Integers.
      *
@@ -18,36 +17,46 @@ public class Lab {
      * Some common implementations of Set are HashSet and TreeSet. Look into how to instantiate them.
      * @return a new Set<Integer> object.
      */
-    public Set<Integer> createSet(){
-        return null;
+    public Set<Integer> createSet() {
+        return new HashSet<>(); // Return a new HashSet instance
     }
 
     /**
      * Return the size of a set.
      * @param set The set that we will be manipulating.
-     * @return the size of set. (number of values)
+     * @return the size of the set. (number of values)
+     * @throws IllegalArgumentException if the set is null
      */
-    public int getSize(Set<Integer> set){
-        return 0;
+    public int getSize(Set<Integer> set) {
+        if (set == null) {
+            throw new IllegalArgumentException("Set cannot be null");
+        }
+        return set.size();
     }
 
     /**
      * Add a new, unique item to a set. Sets can only store unique items (no duplicates).
      * @param set The set that we will be manipulating.
      * @param item The item which we will be adding to set.
-     * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
+     * @throws IllegalArgumentException if the set is null
      */
-    public void addUniqueItem(Set<Integer> set, int item){
-
+    public void addUniqueItem(Set<Integer> set, int item) {
+        if (set == null) {
+            throw new IllegalArgumentException("Set cannot be null");
+        }
+        set.add(item); // Add the item to the set
     }
 
     /**
      * Remove an item from a set.
      * @param set The set that we will be manipulating.
      * @param item The item which we will be removing from set.
-     * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
+     * @throws IllegalArgumentException if the set is null
      */
-    public void removeItem(Set<Integer> set, int item){
-
+    public void removeItem(Set<Integer> set, int item) {
+        if (set == null) {
+            throw new IllegalArgumentException("Set cannot be null");
+        }
+        set.remove(item); // Remove the item from the set
     }
 }
